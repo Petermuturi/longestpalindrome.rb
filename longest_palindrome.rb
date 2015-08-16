4521285dd0357a17dd5b0ee398dde03bd887a5e4
Peter Muturi <pmuturi65@gmail.com>
2015-08-16 17:46:25 +0300

    longest pal

---
+++ longest_palindrome.rb
@@ -0,0 +1,34 @@
+def palindrome?(string)
+  i = 0
+  while i < string.length
+    if string[i] != string[(string.length - 1) - i]
+      return false
+    end
+
+    i += 1
+  end
+
+  return true
+end
+
+def longest_palindrome(string)
+  best_palindrome = nil
+
+  x = 0
+  while idx1 < string.length
+    length = 1
+    while (idx1 + length) <= string.length
+      substring = string.slice(idx1, length)
+
+      if palindrome?(substring) && (best_palindrome == nil || substring.length > best_palindrome.length)
+        best_palindrome = substring
+      end
+
+      length += 1
+    end
+
+    x += 1
+  end
+
+  return best_palindrome
+end
